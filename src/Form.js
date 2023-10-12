5f8215b0134446637dca1228f762051d7b2dc962
Juli <julianalicode@gmail.com>
2023-10-12 15:10:00 -0400

    Add an inputFocus using useRef

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import {useRef} from 'react';
+
+export default function Form(){
+    const inputRef = useRef(null);
+    
+    function handleClick(){
+        inputRef.current.focus();
+    }
+    return (
+        <>
+        <input ref={inputRef} />
+        <button onClick={handleClick}>Focuse the input</button>
+        </>
+    );
+}
